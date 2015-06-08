bf52de55812e6d736acf1b72f428228af8e0beb2
Karsten Huneycutt <kph@alumni.duke.edu>
2015-06-08 13:10:08 -0400

    ugh syntax

--- lib/rack/handler/reel.rb
+++ lib/rack/handler/reel.rb
@@ -15,7 +15,7 @@ module Rack
         app = Rack::CommonLogger.new(app, STDOUT) unless options[:quiet]
         ENV['RACK_ENV'] = options[:environment].to_s if options[:environment]
 
-        supervisor = ::Reel::Rack::Server.supervise as: :reel_rack_server, args: [ app, options ])
+        supervisor = ::Reel::Rack::Server.supervise as: :reel_rack_server, args: [ app, options ]
 
         begin
           sleep
